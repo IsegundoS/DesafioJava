232675fbb991b7b8e51561affab9075d0736b137
Israel Segundo <isegundo@superq.com.mx>
2018-10-04 12:35:50 -0500

    Se agrega Try Catch

--- src/Desafio.java
+++ src/Desafio.java
@@ -2,14 +2,17 @@ public class Desafio {
     public static void main(String[] args) {
         //string cadenaSalida="";
         //System.out.println("Desafio");
-        Negocio desafio = new Negocio();
-        //desafio.setCadena("1o2o3o4o5o");
-        desafio.setCadena("Hola mundo");
-        System.out.println("Cadena Original "+ desafio.getCadena());
-        System.out.println("Convertir: "+desafio.convertir(desafio.getCadena()));
-        //System.out.println(desafio.convertir("1o2o3o4o5o"));
+        try {
+            Negocio desafio = new Negocio();
+            desafio.setCadena("1o2o3o4o5o");
+            //desafio.setCadena("Hola mundo");
+            System.out.println("Cadena Original " + desafio.getCadena());
+            System.out.println("Convertir: " + desafio.convertir(desafio.getCadena()));
+            //System.out.println(desafio.convertir("1o2o3o4o5o"));
 
-        //System.out.println(desafio.getCadena());
-
+            //System.out.println(desafio.getCadena());
+        }catch (Exception Ex){
+            System.out.println(Ex.getMessage());
+        }
     }
 }
